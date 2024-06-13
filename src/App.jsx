20887c4ddb3fe6c16bedb90dbde299ea0a2514cc
konal01 <136685299+konal01@users.noreply.github.com>
2024-06-13 06:41:24 +0530

    fghfghgf

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
      <div id= "choosecolour" className="w-screen h-screen duration-200 font-custom flex items-center justify-center text-5xl " 
         style={{backgroundColor: color}}>
-            <div className='px-10 flex align-middle font-custom'>Choose Your Colour</div>
+            <div className='px-10 flex align-middle font-custom'>Choose Your Color</div>
      </div>
       <div className='fixed flex flex-wrap justify-center  bottom-12 inset-x-0 px-4' >
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
